738c972f42f52c60f21880f150169e1cba3cf97a
Eric from BigD <exz1199@gmail.com>
2017-01-14 04:46:53 +0000

    AJAX working and semi-working links

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 var textSearch = document.getElementById("text");
 var searchBtn = document.getElementById("search");
+
 searchBtn.addEventListener("click", clickedBtn);
 
-function accessWikiDatabase(clickedBtn) {
-  console.log(clickedBtn);
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=opensearch&search=new%20york&format=json&origin=*", true);
-  xhr.send();
-  xhr.onreadystatechange = processServerResponse;
-    
-    function processServerResponse () {
-      var response;
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        response = xhr.responseText;
-        console.log(response);        
-      }
-      textSearch.value.length = "";
-    };
-}
 
-function clickedBtn() {
-  function rdyQueryString() {
-    var rdyString = "";
-    var len = textSearch.value.length;
-    var queryString = textSearch.value;
-    for (var i = 0; i < len; i++) {
-      if (queryString[i] === " ") {
-        rdyString += "%20";
-      }
-      else {
-        rdyString += queryString[i];
+
+function clickedBtn(event) {
+  event.preventDefault();
+  var rdyString = "";
+  var len = textSearch.value.length;
+  var queryString = textSearch.value;
+  for (var i = 0; i < len; i++) {
+    if (queryString[i] === " ") {
+      rdyString += "%20";
+    }
+    else {
+      rdyString += queryString[i];
+    }
+  }
+  textSearch.value = ""; 
+  function getWiki() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&meta=&srsearch=' + rdyString + '&origin=*', true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var data = xhr.responseText;
+        var json = JSON.parse(data);
+        var mainDiv = document.getElementById("test");
+        var a = document.getElementsByTagName("a");
+        for (var i = 0; i < json.query.search.length; i++) {
+          a[i].innerHTML = json.query.search[i].snippet;
+          a[i].href = window.open("https://en.wikipedia.org/wiki/Dog");
+          a[i].target = "_blank";
+        }   
       }
     }
-    console.log(rdyString);
-    return rdyString;  
   }
-  rdyQueryString();
+  getWiki();
 }
